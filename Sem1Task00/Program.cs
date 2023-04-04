a3158db06945c6dc313bcef002e5aae9f509c2bb
Vladimir Kasparovich <volxv-1@ya.ru>
2023-04-04 23:14:48 +0300

    add Task 00 Example

---
+++ Sem1Task00/Program.cs
@@ -0,0 +1,19 @@
+﻿// Считываем данные с консоли
+string? inLine = Console.ReadLine();
+
+if (inLine != null)
+{
+
+    //Парсим введенное число
+
+    int inNumber = int.Parse(inLine);
+
+    //Находим квадрат числа
+
+    int outNumber = inNumber * inNumber;
+
+    //Выводим данные в консоль
+    
+    Console.WriteLine(outNumber);
+
+}
