b222a607365a24f90c1b6e3dbaf8ea7fdcd6f55d
Vladimir Kasparovich <volxv-1@ya.ru>
2023-04-04 23:57:33 +0300

    add Task 002  and Task 002-1 day of week from number

---
+++ Sem1Task002/Program.cs
@@ -0,0 +1,22 @@
+﻿// Считываем данные с консоли
+Console.WriteLine("Enter day weak");
+string? dayLine = Console.ReadLine();
+if (dayLine != null)
+{
+    string[] dayWeek = new string[7];
+    dayWeek[0] = "Monday";
+    dayWeek[1] = "Tuesday";
+    dayWeek[2] = "Wednesday";
+    dayWeek[3] = "Thursday";
+    dayWeek[4] = "Fraiday";
+    dayWeek[5] = "Saturday";
+    dayWeek[6] = "Sanday";
+
+    //Парсим введенное число
+    int day = int.Parse(dayLine);
+    Console.WriteLine(dayWeek[day -1]);
+    
+}
+
+
+
