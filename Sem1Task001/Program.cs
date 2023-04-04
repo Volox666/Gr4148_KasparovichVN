a5fb10e36d2be3fce3ae56f046c6656430078a4d
Vladimir Kasparovich <volxv-1@ya.ru>
2023-04-04 23:16:40 +0300

    add Task 001 проверка числа и его квадрата

---
+++ Sem1Task001/Program.cs
@@ -0,0 +1,19 @@
+﻿// Считываем данные с консоли
+string numLine1 = Console.ReadLine();
+string numLine2 = Console.ReadLine();
+if ((numLine1 != null) && (numLine2 != null))
+{
+    //Парсим введенное число
+    int num1 = int.Parse(numLine1);
+    int num2 = int.Parse(numLine2);
+
+    if (num1 == num2 * num2)
+    {
+        Console.WriteLine("Первое число квадрат второго");
+    }
+    else
+    {
+        Console.WriteLine("Первое число не квадрат второго");
+    }
+
+}
