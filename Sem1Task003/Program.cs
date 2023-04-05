4da127ae7ad3993feb4e6f8f6287f3158eb801ae
Vladimir Kasparovich <volxv-1@ya.ru>
2023-04-05 20:20:08 +0300

    Changes names of Task

--- Sem1Task003/Program.cs
+++ Sem1Task003/Program.cs
@@ -1,19 +1,23 @@
-﻿// Считываем данные с консоли
-Console.WriteLine("Enter ");
-string? numNLine = Console.ReadLine();
-// Проверяем что не пустые данные
-if (numNLine != null)
+﻿using System;
+// Считываем данные с консоли
+Console.WriteLine("Enter day weak");
+string? dayLine = Console.ReadLine();
+if (dayLine != null)
 {
+    string[] dayWeek = new string[7];
+    dayWeek[0] = "Monday";
+    dayWeek[1] = "Tuesday";
+    dayWeek[2] = "Wednesday";
+    dayWeek[3] = "Thursday";
+    dayWeek[4] = "Fraiday";
+    dayWeek[5] = "Saturday";
+    dayWeek[6] = "Sanday";
+
     //Парсим введенное число
-    int numN = int.Parse(numNLine);
-    int startNum = numN * (-1);
-    //строка куда собирается результат, пустая
-    string res = String.Empty;
-    while (startNum <= numN)
-    {
-        res = res + startNum + " ";
-        startNum = startNum + 1; //startNumm++;
-    }
-    //Выводим на консоль
-    Console.WriteLine(res);
+    int day = int.Parse(dayLine);
+    Console.WriteLine(dayWeek[day -1]);
+    
 }
+
+
+
