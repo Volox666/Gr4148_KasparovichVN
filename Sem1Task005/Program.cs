2ff44fd52ff1cd71a6caeeef21c255a4e5ced266
Vladimir Kasparovich <volxv-1@ya.ru>
2023-04-05 20:21:45 +0300

    Changes names of Task

---
+++ Sem1Task005/Program.cs
@@ -0,0 +1,19 @@
+﻿// Считываем данные с консоли
+Console.WriteLine("Enter ");
+string? numNLine = Console.ReadLine();
+// Проверяем что не пустые данные
+if (numNLine != null)
+{
+    //Парсим введенное число
+    int numN = int.Parse(numNLine);
+    int startNum = numN * (-1);
+    //строка куда собирается результат, пустая
+    string res = String.Empty;
+    while (startNum <= numN)
+    {
+        res = res + startNum + " ";
+        startNum = startNum + 1; //startNumm++;
+    }
+    //Выводим на консоль
+    Console.WriteLine(res);
+}
