28f84dd3a4499c65245bcddf52fefb740b96a899
Volox666 <126723137+Volox666@users.noreply.github.com>
2023-04-06 13:15:58 +0300

    Update Program.cs

--- Sem1Task008/Program.cs
+++ Sem1Task008/Program.cs
@@ -2,3 +2,23 @@
 // все чётные числа от 1 до N.
 //  5 -> 2, 4
 //  8 -> 2, 4, 6, 8
+
+using System;
+namespace project{
+  class Program{
+    static void Main(){
+     
+      Console.WriteLine("Введите число");
+      int num = Convert.ToInt32(Console.ReadLine());  // Проверка на не пустой символ и перевод в инт 
+      
+      int count = 2;
+      while(count<=num) 
+      {        
+        Console.Write(count+" ");
+        count = count + 2;
+      }
+      
+    
+    }
+  }
+ }
