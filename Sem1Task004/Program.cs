6c6ecada812ecb7c281ae58a755752f64a673611
Volox666 <126723137+Volox666@users.noreply.github.com>
2023-04-06 12:30:59 +0300

    Update Program.cs

--- Sem1Task004/Program.cs
+++ Sem1Task004/Program.cs
@@ -3,3 +3,25 @@
 //  2, 3, 7 -> 7
 //  44 5 78 -> 78
 //  22 3 9 -> 22
+
+using System;
+namespace project{
+  class Program{
+    static void Main(){
+     
+      Console.WriteLine("Введите первое число");
+      int num1 = Convert.ToInt32(Console.ReadLine());
+      
+      Console.WriteLine("Введите второе число");
+      int num2 = Convert.ToInt32(Console.ReadLine());
+
+      Console.WriteLine("Введите третье число");
+      int num3 = Convert.ToInt32(Console.ReadLine());
+
+      int maxNum = Math.Max(Math.Max(num1, num2), num3); //Math.Max() метод возвращает большее из двух чисел
+    
+      Console.WriteLine("Максимальное число: " + maxNum);
+    
+    }
+  }
+ }
