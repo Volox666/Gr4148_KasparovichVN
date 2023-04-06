00a22a37177b46978a83f3d39281a54eb024f984
Volox666 <126723137+Volox666@users.noreply.github.com>
2023-04-06 12:13:07 +0300

    Update Program.cs

--- Sem1Task002/Program.cs
+++ Sem1Task002/Program.cs
@@ -4,3 +4,19 @@
 //  a = 2 b = 10 -> max = 10
 //  a = -9 b = -3 -> max = -3
 
+using System;
+namespace project{
+  class Program{
+    static void Main(){
+     
+      Console.WriteLine("Введите первое число");
+      int num1 = Convert.ToInt32(Console.ReadLine());
+      
+      Console.WriteLine("Введите второе число");
+      int num2 = Convert.ToInt32(Console.ReadLine());
+      
+      Console.WriteLine("Минимальное число: " + Math.Min(num1, num2));
+    
+    }
+  }
+ }
