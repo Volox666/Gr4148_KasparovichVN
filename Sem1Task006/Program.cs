49977b90f022cee7670aff71b4f97c3dc054c28c
Volox666 <126723137+Volox666@users.noreply.github.com>
2023-04-06 13:07:11 +0300

    Update Program.cs

--- Sem1Task006/Program.cs
+++ Sem1Task006/Program.cs
@@ -3,3 +3,20 @@
 //  4 -> да
 //  -3 -> нет
 //  7 -> нет
+
+using System;
+namespace project{
+  class Program{
+    static void Main(){
+     
+      Console.WriteLine("Введите число");
+      int num = Convert.ToInt32(Console.ReadLine());  // Проверка на не пустой символ и перевод в инт 
+
+bool res = (num%2 == 0);
+
+if(res) System.Console.WriteLine("Число четное");
+      else System.Console.WriteLine("Число не четное");
+    
+    }
+  }
+ }
