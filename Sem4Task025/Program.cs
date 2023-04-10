41f8c91e88bc185860ba4bedd49fc7dd7d56108f
Vladimir Kasparovich <volxv-1@ya.ru>
2023-04-10 17:44:24 +0300

    Add Seminar 4 HW

---
+++ Sem4Task025/Program.cs
@@ -0,0 +1,41 @@
+﻿// №25 Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в
+// натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+// * Написать калькулятор с операциями +, -, /, * и возведение в степень
+
+using System;
+namespace project
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            int ReadData(string msg) // Метод читает данные от пользователя
+            {
+                System.Console.WriteLine(msg);
+                int res = int.Parse(Console.ReadLine() ?? "0");
+                return res;
+            }
+
+            int Step(int osnow, int pow) //метод возводит в степень
+            {
+                int powRez = 1;
+                for (int i = 0; i < pow; i++) powRez *= osnow;
+                return powRez;
+            }
+
+            int osnow = ReadData("Введите основание: ");
+            int pow = ReadData("Введите степень: ");
+            if (pow < 0) Console.WriteLine("Показатель степени должен быть положительный");
+            else
+            {
+                int rez = Step(osnow, pow);
+                Console.WriteLine("Число {0} в степени {1} равно: {2}", osnow, pow, rez);
+            }
+
+
+        }
+    }
+}
---
+++ Sem4Task027/Program.cs
@@ -0,0 +1,39 @@
+﻿// №27 Напишите программу, которая принимает на вход число и выдаёт сумму цифр в
+// числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+// * Сделать оценку времени алгоритма через вещественные числа и строки
+
+
+using System;
+namespace project
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            int ReadData(string msg) // Метод читает данные от пользователя
+            {
+                System.Console.WriteLine(msg);
+                int res = int.Parse(Console.ReadLine() ?? "0");
+                return res;
+            }
+            int Sum(int num)
+            {
+                int res = 0;
+                while (num > 0)
+                {
+                    res += num % 10;   // Берет последнюю цифру
+                    num = num / 10;    // уменьшает число на разряд
+                }
+                return res;
+            }
+            int num = ReadData("Введите число: ");
+            Console.WriteLine($"Сумма цифр в числе: { Sum(num)}");
+
+
+        }
+    }
+}
